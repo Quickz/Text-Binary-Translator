b4622667017a4d097b78cf478cecd235a3f92f9b
Alex <quickz@inbox.lv>
2017-02-07 23:51:44 +0200

    added the code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Text-Binary-Translator
 An app that translates text into binary values and binary values into text.
+<a href="https://quickz.github.io/Text-Binary-Translator/" target="_blank">Click here</a> to check it out.
---
+++ script.js
@@ -0,0 +1,102 @@
+(function(){
+	
+	
+	/**
+	 * global values
+	 *
+	*/
+	var input = document.getElementById("text-input");
+	var output = document.getElementById("text-output");
+	var copy = document.getElementById("copy");
+	var swap = document.getElementById("swap");
+	var toBinary = true;
+
+
+	/**
+	 * input field edit event
+	 *
+	*/
+	input.oninput = function() {
+
+		var text = input.value;
+
+		if (toBinary)
+			text = GetBinary(text);
+		else
+			text = GetText(text);
+
+		output.innerHTML = text;
+
+	};
+	
+
+	/**
+	 * copies all of the output content
+	 *
+	*/
+	copy.onclick = function() {
+
+		if (document.selection)
+		{
+            var range = document.body.createTextRange();
+            range.moveToElementText(output);
+            range.select();
+        }
+        else if (window.getSelection)
+        {
+            var range = document.createRange();
+            range.selectNode(output);
+            window.getSelection().addRange(range);
+        }
+        document.execCommand("copy");
+	}
+
+
+	/**
+	 * swaps input, output content
+	 * and turns the conversion the other way
+	*/
+	swap.onclick = function() {
+
+		toBinary = !toBinary;
+		var text = output.innerHTML;
+		output.innerHTML = input.value;
+		input.value = text;
+
+	};
+
+
+	/**
+	 * returns binary value of a string
+	 *
+	*/
+	function GetBinary(text)
+	{
+		var output = "";
+		for (var i = 0 ; i < text.length; i++)
+		{
+			var charCode = text.charCodeAt(i);
+			output += charCode.toString(2) + " ";
+		}
+		return output;
+	}
+
+
+	/**
+	 * returns string value of a binary
+	 *
+	*/
+	function GetText(text)
+	{
+		text = text.split(" ");
+		var output = "";
+		for (var i = 0 ; i < text.length; i++)
+		{
+			var symbol = parseInt(text[i], 2);
+			output += String.fromCharCode(symbol);
+		}
+		return output;
+	}
+
+
+})();
